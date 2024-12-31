e09bbb9cb3118c7715ff4f6129a6686ddde01bdb
Himanshu Lodha <himanshulodha2002@gmail.com>
2024-12-31 09:50:53 +0000

    test

--- components/intro.tsx
+++ components/intro.tsx
@@ -71,7 +71,7 @@ export default function Intro() {
         web development to deliver impactful digital solutions.
       </motion.h1> */}
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium leading-relaxed sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
